f909e1f985fbb8affe08634dd574c1279fd79b0b
alexgoni <goni000211@gmail.com>
2025-04-23 11:07:21 +0900

    feat: market 페이지에 cocursor 적용

--- src/pages/(market)/market/index.tsx
+++ src/pages/(market)/market/index.tsx
@@ -1,3 +1,4 @@
+import CoCursorProvider from "cocursor";
 import Footer from "components/Layout/Footer";
 import MainLayout from "components/Layout/MainLayout";
 import Navbar from "components/Layout/Navbar";
@@ -7,13 +8,16 @@ import BestProductsSection from "./components/BestProductsSection";
 
 export default function Market() {
   return (
-    <>
+    <CoCursorProvider
+      apiKey={process.env.REACT_APP_COCURSOR_API_KEY as string}
+      channel="market"
+    >
       <Navbar />
       <MainLayout>
         <BestProductsSection />
         <AllProductsSection />
       </MainLayout>
       <Footer />
-    </>
+    </CoCursorProvider>
   );
 }
